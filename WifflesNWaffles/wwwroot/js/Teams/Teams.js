699d1e507a1d1397d41cc08d5dfd923f9bdeafe2
unknown <connor.wilson48@gmail.com>
2024-09-26 17:01:29 -0700

    Removed generate button after it's clicked

--- WifflesNWaffles/wwwroot/js/Teams/Teams.js
+++ WifflesNWaffles/wwwroot/js/Teams/Teams.js
@@ -3,11 +3,7 @@ const elements = {
 
     generateButton: $('[data-selector="teams"]'),
     teamsDiv: $('[data-selector="teamsDiv"]'),
-
-
 }
-
-
 const initialize = function () {
 
     elements.generateButton.click(generateTeams);
@@ -16,6 +12,7 @@ const initialize = function () {
 
 const generateTeams = function () {
 
+    elements.generateButton.css("display", "none");
     elements.teamsDiv.css("display", "");
 
 }
