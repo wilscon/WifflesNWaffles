092dea577148cb436564170c5df5c4c27bb44f10
unknown <connor.wilson48@gmail.com>
2024-10-02 11:21:19 -0700

    Added functionality so 2022 attendees could be added to attendees table with null email address and phone number

---
+++ WifflesNWaffles/wwwroot/js/Attendees/Attendees.js
@@ -0,0 +1,32 @@
+﻿const elements = {
+
+   yearDropdown: $('#yearDropdown'),
+
+}
+
+const initialize = function () {
+
+    elements.yearDropdown.on("change", changeYear);
+
+}
+
+const changeYear = function () {
+
+    let year = $('#yearDropdown').val();
+
+    $.ajax({
+        url: '/Home/ChangeYear',
+        type: 'GET',
+        data: {year: year},
+        success: function (partialViewResult) {
+          
+
+                $('#attendeesContainer').html(partialViewResult); 
+        },
+        error: function (xhr, status, error) {
+            alert("An error occurred: " + error);
+        }
+    });
+}
+
+initialize();
