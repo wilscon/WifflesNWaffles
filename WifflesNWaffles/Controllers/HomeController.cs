a66ab62febf604f373512e273dd1080e786c94a6
unknown <connor.wilson48@gmail.com>
2024-10-08 21:31:33 -0700

    Added utility class and new RSVPDBModel extension

--- WifflesNWaffles/Controllers/HomeController.cs
+++ WifflesNWaffles/Controllers/HomeController.cs
@@ -1,19 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Xml;
 using WifflesNWaffles.Data;
 using WifflesNWaffles.Models;
-using WifflesNWaffles.Enums;
-using Twilio;
-using Twilio.Rest.Api.V2010.Account;
-using Twilio.Types;
-using static Azure.Core.HttpHeader;
-using static System.Net.Mime.MediaTypeNames;
 using WifflesNWaffles.Extensions;
 
 namespace WifflesNWaffles.Controllers
@@ -22,9 +11,6 @@ namespace WifflesNWaffles.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
-
-        
-
 
         public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
@@ -40,37 +26,31 @@ namespace WifflesNWaffles.Controllers
         [HttpGet]
         public async Task<IActionResult> Attendees() {
 
-            IEnumerable<RSVPDBModel> attendees = await _context.attendees.Where(a => a.Year == "2024").ToListAsync();
-            List<AttendeesModel> people = new List<AttendeesModel> {};
+            IEnumerable<RSVPDBModel> attendeesDB = await _context.attendees.Where(a => a.Year == "2024").ToListAsync();
+            List<AttendeesModel> attendees = new List<AttendeesModel> {};
             
-            foreach (var attendee in attendees)
+            foreach (var attendee in attendeesDB)
             {
-                string base64Image = GenerateProfileImageBase64(attendee.FirstName[0].ToString() + attendee.LastName[0].ToString(), 40, 40);
-
-                people.Add(new AttendeesModel { FirstName = attendee.FirstName, LastName = attendee.LastName, Topping = attendee.Topping, Attendance = (AttendanceStatus)attendee.Attendance, ProfileImage = base64Image });
-               
+                attendees.Add(attendee.toAttendeesModel());  
             }
 
-            return View("Attendees",people);
+            return View("Attendees",attendees);
         }
 
         [HttpGet]
         public async Task<IActionResult> ChangeYear(string year) {
 
 
-            IEnumerable<RSVPDBModel> attendees = await _context.attendees.Where(a => a.Year == year).ToListAsync();
+            IEnumerable<RSVPDBModel> attendeesDB = await _context.attendees.Where(a => a.Year == year).ToListAsync();
 
-            List<AttendeesModel> people = new List<AttendeesModel> { };
+            List<AttendeesModel> attendees = new List<AttendeesModel> { };
 
-            foreach (var attendee in attendees)
+            foreach (var attendee in attendeesDB)
             {
-                string base64Image = GenerateProfileImageBase64(attendee.FirstName[0].ToString() + attendee.LastName[0].ToString(), 40, 40);
-
-                people.Add(new AttendeesModel { FirstName = attendee.FirstName, LastName = attendee.LastName, Topping = attendee.Topping, Attendance = (AttendanceStatus)attendee.Attendance, ProfileImage = base64Image });
-
+                attendees.Add(attendee.toAttendeesModel());
             }
 
-            return PartialView("Views/Home/_AttendeesTable.cshtml", people);
+            return PartialView("Views/Home/_AttendeesTable.cshtml", attendees);
 
         }
 
@@ -89,18 +69,14 @@ namespace WifflesNWaffles.Controllers
         [HttpGet]
         public async Task<IActionResult> RSVP() {
 
-
             return View("RSVP");
         }
 
         [HttpGet]
         public async Task<IActionResult> Teams() {
 
-            
-
             List<RSVPDBModel> attendees = await _context.attendees.ToListAsync();
-            
-
+           
             TeamsModel teams = new TeamsModel();
             List<AttendeesModel> Team1;
             List<AttendeesModel> Team2;
@@ -110,9 +86,6 @@ namespace WifflesNWaffles.Controllers
             teams.Team1Name = TeamNameGenerator.GenerateRandomTeamName();
             teams.Team2Name = TeamNameGenerator.GenerateRandomTeamName();
 
-            
-            
-           
             return View("Teams",teams);
         }
 
@@ -144,8 +117,6 @@ namespace WifflesNWaffles.Controllers
                 Console.WriteLine("An unexpected error occurred: " + ex.Message);
             }
 
-
-
             return Ok(new { success = true, message = "RSVP form received successfully." });
         }
 
@@ -160,56 +131,6 @@ namespace WifflesNWaffles.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public static string GenerateProfileImageBase64(string initials, int width, int height)
-        {
-            using (Bitmap bitmap = GenerateProfileImage(initials, width, height))
-            {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    // Save the image to the memory stream as PNG
-                    bitmap.Save(ms, ImageFormat.Png);
-
-                    // Convert the byte array to Base64 string
-                    byte[] imageBytes = ms.ToArray();
-                    string base64String = Convert.ToBase64String(imageBytes);
-
-                    return "data:image/png;base64," + base64String;
-                }
-            }
-        }
-
-        public static Bitmap GenerateProfileImage(string initials, int width, int height)
-        {
-            Random random = new Random();
-
-            // Create a blank canvas
-            Bitmap bitmap = new Bitmap(width, height);
-
-            // Create random background color
-            Color backgroundColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
-
-            // Fill the canvas with the background color
-            using (Graphics g = Graphics.FromImage(bitmap))
-            {
-                g.Clear(backgroundColor);
-
-                // Create font for initials
-                System.Drawing.Font font = new System.Drawing.Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Pixel);
-
-                // Set text color (contrast to background)
-                Brush textBrush = new SolidBrush(Color.White);
-
-                // Measure the size of the initials text
-                SizeF textSize = g.MeasureString(initials, font);
-
-                // Draw the initials in the center of the image
-                g.DrawString(initials, font, textBrush,
-                             (width - textSize.Width) / 2,
-                             (height - textSize.Height) / 2);
-            }
-
-            return bitmap;
-        }
     }
 
     
---
+++ WifflesNWaffles/Utilities/ProfileImageGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace WifflesNWaffles.Utilities
+{
+    public static class ProfileImageGenerator
+    {
+        public static string GenerateProfileImageBase64(string initials, int width, int height)
+        {
+            using (Bitmap bitmap = GenerateProfileImage(initials, width, height))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Save the image to the memory stream as PNG
+                    bitmap.Save(ms, ImageFormat.Png);
+
+                    // Convert the byte array to Base64 string
+                    byte[] imageBytes = ms.ToArray();
+                    string base64String = Convert.ToBase64String(imageBytes);
+
+                    return "data:image/png;base64," + base64String;
+                }
+            }
+        }
+
+        public static Bitmap GenerateProfileImage(string initials, int width, int height)
+        {
+            Random random = new Random();
+
+            // Create a blank canvas
+            Bitmap bitmap = new Bitmap(width, height);
+
+            // Create random background color
+            Color backgroundColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+            // Fill the canvas with the background color
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(backgroundColor);
+
+                // Create font for initials
+                System.Drawing.Font font = new System.Drawing.Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                // Set text color (contrast to background)
+                Brush textBrush = new SolidBrush(Color.White);
+
+                // Measure the size of the initials text
+                SizeF textSize = g.MeasureString(initials, font);
+
+                // Draw the initials in the center of the image
+                g.DrawString(initials, font, textBrush,
+                             (width - textSize.Width) / 2,
+                             (height - textSize.Height) / 2);
+            }
+
+            return bitmap;
+        }
+
+    }
+}
